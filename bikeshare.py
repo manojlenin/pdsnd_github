48c49a8f4ad849ec1cecfffa714aad1cc17de581
leni004 <manoj.lenin@bertelsmann.de>
2023-05-29 19:36:59 +0800

    Updated comment to City_data dictionary

--- bikeshare.py
+++ bikeshare.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import sys
 
-""" By Manoj:- Dictionary to store data for csv file for respecitve city. Key hold the city name and value with filename"""
+""" By Manoj:- Dictionary to store data for csv file for respecitve city. Key hold the city name and value with filename. The files are saved under Datafolder"""
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
